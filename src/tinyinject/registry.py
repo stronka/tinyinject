5d7f4eec26fe5abc4bec0a31b40cb1cd9c5e4bff
chrsec <stronka.k@gmail.com>
2024-01-31 17:50:17 +0100

    Implement Registry

---
+++ src/tinyinject/registry.py
@@ -0,0 +1,23 @@
+_registry_data = {}
+
+
+class Registry:
+    @staticmethod
+    def implements(*, interface: type):
+        def wrapper(implementation: type):
+            _registry_data[interface] = implementation
+            return implementation
+
+        return wrapper
+
+    @property
+    def data(self):
+        return _registry_data
+
+    def get(self, interface):
+        maybe_type = _registry_data[interface]
+
+        if isinstance(maybe_type, type):
+            return maybe_type()
+
+        return maybe_type
---
+++ tests/test_registry.py
@@ -0,0 +1,51 @@
+from typing import Protocol
+
+import pytest
+
+from src.tinyinject.registry import Registry
+
+
+class ExampleType(Protocol):
+    """Test example"""
+
+
+class ExampleCallable(Protocol):
+    def __call__(self, *args, **kwargs) -> int:
+        ...
+
+
+class TestRegistry:
+    @pytest.fixture
+    def registry(self):
+        return Registry()
+
+    def test_implements_case_type_then_register_it(self, registry):
+        @registry.implements(interface=ExampleType)
+        class ExampleTypeImpl:
+            pass
+
+        assert registry.data[ExampleType] == ExampleTypeImpl
+
+    def test_get_case_type_then_return_instance(self, registry):
+        @registry.implements(interface=ExampleType)
+        class ExampleTypeImpl:
+            pass
+
+        assert isinstance(registry.get(ExampleType), ExampleTypeImpl)
+
+    def test_implements_case_callable_interface_then_can_use_function(self, registry):
+        @registry.implements(interface=ExampleCallable)
+        def callable_impl():
+            return 42
+
+        assert registry.get(ExampleCallable)() == 42
+
+    def test_implements_case_callable_implemented_by_callable_then_behaves_as_function(self, registry):
+        @registry.implements(interface=ExampleCallable)
+        class Callable:
+            def __call__(self, *args, **kwargs):
+                return 42
+
+        assert registry.get(ExampleCallable)() == 42
+
+
