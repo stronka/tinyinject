e472125370a39e122b3041887d1024708fb437e8
chrsec <stronka.k@gmail.com>
2024-02-17 17:45:13 +0100

    Add ruff

--- src/tinyinject/__init__.py
+++ src/tinyinject/__init__.py
@@ -1,6 +1,7 @@
+# ruff: noqa: F401
 from .registry import di
-from .require import Require  # noqa
-from .require import require_kwargs  # noqa
+from .require import Require
+from .require import require_kwargs
 from .context import override
 
 
